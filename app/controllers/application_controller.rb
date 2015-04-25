c85cbb7dd8946c1e23236b82b7e59825af06efce
Samel Martinucci <samuelmartinucci@gmail.com>
2015-04-24 23:11:06 -0300

    Correções

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  after_filter :set_csrf_cookie
 end
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module TimeTracker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
-    config.api_only = false
+    # config.api_only = false
+
+    config.middleware.insert_after(ActiveRecord::QueryCache, ActionDispatch::Cookies)
+    config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
   end
 end
